5dc59993890edb0bbeaa6dcee1b6c9f4a7e55c27
JoaoSchrock <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-13 21:31:19 -0300

    Add files via upload

---
+++ Iteração/IteracaoWhile.java
@@ -0,0 +1,44 @@
+package com.company.Interacao;
+
+
+public class IteracaoWhile {
+
+    public static void main(String[] args) {
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println("Um texto qualquer.");
+//		}
+//
+//		int i = 0;
+//		while (i < 10) {
+//			System.out.println("Um texto qualquer.");
+//			i++;
+//		}
+
+
+
+//		int i = 0;
+//		while (i < 10) {
+//			if (i == 5) {
+//				System.out.println("Vai parar!");
+//				break;
+//			}
+//			System.out.println(i + ": Um texto qualquer.");
+//			i++;
+//		}
+
+
+
+//        int i = 0;
+//        while (i < 10) {
+//            if (i == 5) {
+//                System.out.println("Vai continuar...");
+//                i++;
+//                continue;
+//            }
+//
+//            System.out.println(i + ": Um texto qualquer.");
+//            i++;
+//        }
+    }
+
+}
---
+++ Iteração/exercicio01.java
@@ -0,0 +1,24 @@
+// Integer[] numeros = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+//Encontre os números divisíveis por 3.
+
+package com.company.Interacao;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("_OPERADORES DIVISIVEIS POR 3 COM (for)");
+        int i;
+        for ( i = 0; i < 20; i++)
+            if (i % 3 == 0) {
+                System.out.println(i);
+                }
+
+
+
+            }
+
+    }
+
