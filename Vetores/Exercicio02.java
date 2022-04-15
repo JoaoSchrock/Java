d9224d0390c1bfd38363b841cfd7f4b470bdd92e
JoaoSchrock <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-15 12:38:28 -0300

    Add files via upload

---
+++ Vetores/Exercicio02.java
@@ -0,0 +1,24 @@
+package com.company.vetores;
+
+/*
+
+Repare nos vetores abaixo:
+
+Double[] semanaUm = new Double[]{ 1000.0, 1500.0, 1250.0, 2100.0, 1350.0, 970.0, 0.0 };
+Double[] semanaDois = new Double[]{ 1050.0, 2500.0, 3250.0, 1100.0, 1430.0, 900.0, 0.0 };
+Double[] semanaTres = new Double[]{ 1700.0, 1300.0, 1450.0, 2000.0, 1390.0, 800.0, 0.0 };
+Double[] semanaQuatro = new Double[]{ 2100.0, 1600.0, 1850.0, 2100.0, 1220.0, 998.0, 0.0 };
+
+Double[][] mes = new Double[][] { semanaUm, semanaDois, semanaTres, semanaQuatro };
+Os quatro primeiros vetores estão guardando o faturamento diário de cada semana e o último
+vetor, que é de 2 dimensões,
+guarda o faturamento mensal separado por semanas.
+
+Crie um algoritmo que encontre qual a semana teve o maior faturamento.
+No final mostre qual foi essa semana e de quanto
+foi esse faturamento.
+
+ */
+
+public class Exercicio02 {
+}
