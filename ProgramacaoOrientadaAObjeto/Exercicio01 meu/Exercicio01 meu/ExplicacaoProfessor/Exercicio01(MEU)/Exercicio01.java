b04d7ef405966a6a76c794bcae6455001cfa549a
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-27 18:46:03 -0300

    Add files via upload

---
+++ ProgramacaoOrientadaAObjeto/Exercicio01 meu/Exercicio01 meu/ExplicacaoProfessor/Exercicio01(MEU)/Exercicio01.java
@@ -0,0 +1,20 @@
+package ProgramacaoOrientadaAObjeto;
+/*Método que informa necessidade de repor estoque
+Crie um programa que contenha um método que verifique a necessidade de repor o estoque de determinado produto em uma
+loja qualquer. Considere que a quantidade mínima de um produto deva ser 10. Se o estoque for menor que isso, então ele
+retornará *true*, pois, será necessária a reposição do estoque, caso contrário, ele retornará false.
+Como parâmetro, esse método deverá receber um tipo que você irá criar e chamar de Produto, ou seja, você vai criar uma
+classe chamada Produto com os atributos necessários. Os atributos da classe serão nome e quantidadeEstoque.
+Feito isso, implemente o método main do programa. Nele você vai criar uma variável do tipo Produto e depois usar essa
+variável para invocar o método que valida a necessidade de reposição de estoque.
+No final, você vai exibir, no console, se é necessária a reposição ou não.
+ */
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Produto1 FoneDeOuvido = new Produto1();
+        FoneDeOuvido.nome = "edifier w800bt";
+        FoneDeOuvido.quantidadeEstoque = 10;
+    }
+
+    }
+
