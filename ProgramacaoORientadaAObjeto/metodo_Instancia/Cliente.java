b9f4a38461595649306fc4d6154b12327d4439be
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-27 21:31:38 -0300

    Add files via upload

---
+++ ProgramacaoORientadaAObjeto/metodo_Instancia/Cliente.java
@@ -0,0 +1,17 @@
+package ProgramacaoOrientadaAObjeto.metodo_Instancia;
+public class Cliente {
+    String primeiroNome;
+    String ultimoNome;
+    String telefone;
+    String email;
+    String obterNomeCompleto() {
+        String nomeCompleto = primeiroNome + " " + ultimoNome;
+        return nomeCompleto;
+    }
+
+    String obterDDD() {
+        String ddd = telefone.substring(0, 2);
+        return ddd;
+    }
+
+}
