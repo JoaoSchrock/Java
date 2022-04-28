848ce9bceac70d294e4cd85a963d06d6fc89a2b2
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-28 19:29:56 -0300

    Update Exercicio02.java

--- ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Exercicio02.java
+++ ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Exercicio02.java
@@ -1,5 +1,20 @@
 /*
 Refatore este código alterando o método estático para um método de instância.
  */
+package ProgramacaoOrientadaAObjeto.Exercicio2;
+
+/*
+Refatore este código alterando o método estático para um método de instância.
+ */
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Produto2 produto2 = new Produto2();
+        produto2.nome = "Xiaome Earbuds Airdots S";
+        produto2.quantidadeEstoque = 10;
+        System.out.println("Há necessidade de ressarcir o estoque do produto " + produto2.nome + "? " + produto2.eNecessarioReporEstoque());
+    }
+
+}
 
 
