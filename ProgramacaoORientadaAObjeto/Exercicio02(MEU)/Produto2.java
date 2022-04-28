91d94ce2fc42e3026ee77b6d0969e97e5b8d27b9
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-28 19:30:45 -0300

    Update Produto2.java

--- ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Produto2.java
+++ ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Produto2.java
@@ -1,8 +1,14 @@
-package ProgramacaoOrientadaAObjeto;
+package ProgramacaoOrientadaAObjeto.Exercicio2;
 
 public class Produto2 {
-       Integer QUANTIDADE_MINIMA_ESTOQUE = 10;// Essa não é uma variável e sim, uma constante.
-        String nome;
-        Integer quantidadeEstoque;
+    static final Integer QUANTIDADE_MINIMA_ESTOQUE = 10;// Essa não é uma variável e sim, uma constante.
+    String nome;
+    Integer quantidadeEstoque;
+
+    Boolean eNecessarioReporEstoque() {
+        if (quantidadeEstoque < Produto2.QUANTIDADE_MINIMA_ESTOQUE) {
+            return true;
+        }
+        return false;
     }
 
