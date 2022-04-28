dbea19ade5372a8e59b41436dc61a2bdd97883d7
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-27 21:42:24 -0300

    Add files via upload

---
+++ ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Exercicio02.java
@@ -0,0 +1,5 @@
+/*
+Refatore este código alterando o método estático para um método de instância.
+ */
+
+
---
+++ ProgramacaoORientadaAObjeto/Exercicio02(MEU)/Produto2.java
@@ -0,0 +1,8 @@
+package ProgramacaoOrientadaAObjeto;
+
+public class Produto2 {
+       Integer QUANTIDADE_MINIMA_ESTOQUE = 10;// Essa não é uma variável e sim, uma constante.
+        String nome;
+        Integer quantidadeEstoque;
+    }
+
