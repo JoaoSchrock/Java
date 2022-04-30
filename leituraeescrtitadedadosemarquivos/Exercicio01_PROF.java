6000aff039a9cc01bedfdddca34b46d79d60ef31
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-29 21:30:56 -0300

    Add files via upload

---
+++ leituraeescrtitadedadosemarquivos/Exercicio01_PROF.java
@@ -0,0 +1,34 @@
+package  leituraeescrtitadedadosemarquivos;
+/*
+Crie uma lista de tarefas e salve a lista em um arquivo
+ */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Exercicio01_PROF {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> linhas = new ArrayList<String>();
+        String tarefa;
+        int i = 0;
+        while (true) { // Deixei o true como condição do laço, pois, o que vai pará-lo é o fato do usuário digitar o "x" no console.
+            System.out.print("Tarefa " + i + ": ");
+            tarefa = scanner.nextLine();
+            if ("x".equals(tarefa)) {
+                break; // Essa é a única maneira de parar esse laço.
+            }
+            linhas.add(tarefa);
+            i++;
+        }
+        escreverNoArquivo("C:\\Users\\Administrador\\Desktop\\arquivo.txt", linhas);
+        scanner.close();
+        System.out.println("Fim...");
+    }
+    static void escreverNoArquivo(String arquivo, ArrayList<String> linhas) throws IOException {
+        Path path = Paths.get(arquivo);
+        Files.write(path, linhas);
+    }
+}
