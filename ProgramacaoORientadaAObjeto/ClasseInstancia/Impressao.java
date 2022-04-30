4d87effd337d9c254e82867b3cff07971f9d5b59
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-04-29 21:25:40 -0300

    Add files via upload

---
+++ ProgramacaoORientadaAObjeto/ClasseInstancia/Impressao.java
@@ -0,0 +1,12 @@
+package ProgramacaoOrientadaAObjeto.ClasseInstancia;
+
+public class Impressao {
+
+    static void informacao(String texto){
+        System.out.println("[INFO] " + texto);
+    }
+
+    static void erro(String texto){
+        System.err.println("[ERRO] " + texto);
+    }
+}
---
+++ ProgramacaoORientadaAObjeto/ClasseInstancia/Produto.java
@@ -0,0 +1,10 @@
+package ProgramacaoOrientadaAObjeto.ClasseInstancia;
+
+public class Produto {
+
+    static Integer quantidadeMinimaEstoque = 1;
+    String nome;
+    String getNome() {
+        return nome;
+    }
+}
