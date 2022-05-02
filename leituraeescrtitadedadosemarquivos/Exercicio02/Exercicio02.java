26a93ea915d3e729e89b9abfcb9e342cc0600c6d
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-02 19:53:08 -0300

    Add files via upload

---
+++ leituraeescrtitadedadosemarquivos/Exercicio02/Exercicio02.java
@@ -0,0 +1,21 @@
+package leituraeescrtitadedadosemarquivos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/*
+Mostre a lista de tarefas para o usuário
+ */
+public class Exercicio02 {
+    public static void main(String[] args) throws IOException {
+        Path arquivo = Paths.get("C:\\Users\\aluno.LABCETEC\\Desktop\\Tarefas.txt");
+        List<String> Lista = Files.readAllLines(arquivo);
+        for(int i = 0; i< Lista.size(); i++){
+            String tarefasUsuario = Lista.get(i);
+            System.out.println(i +"°" + " Tarefas: " + tarefasUsuario);
+        }
+    }
+}
---
+++ leituraeescrtitadedadosemarquivos/Exercicio02/Exercicio02PROF.java
@@ -0,0 +1,27 @@
+package leituraeescrtitadedadosemarquivos;
+
+/*
+Mostre a lista de tarefas para o usuário
+ */
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+public class Exercicio02PROF {
+    public static void main(String[] args) throws IOException {
+        List<String> linhas = lerDoArquivo("/tmp/lista-de-tarefas.txt");
+        ;
+        System.out.println("SUA LISTA DE TAREFAS DO DIA");
+        for (int i = 0; i < linhas.size(); i++) {
+            String tarefa = linhas.get(i);
+            System.out.println(i + ": " + tarefa);
+        }
+        System.out.println("----------");
+    }
+
+    static List<String> lerDoArquivo(String arquivo) throws IOException {
+        Path path = Paths.get(arquivo);
+        return Files.readAllLines(path);
+    }
+}
