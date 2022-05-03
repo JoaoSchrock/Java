c537900ae250815fc730974d86198dc0e6d3a573
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-03 12:34:56 -0300

    Add files via upload

---
+++ algoritmos_avancados/AlgoritmoAvancado.java
@@ -0,0 +1,34 @@
+package com.company.algoritmos_avancados;
+
+public class AlgoritmoAvancado {
+
+	public static void main(String[] args) {
+		ListaAlunos lista = new ListaAlunos();
+		
+		Aluno aluno = new Aluno();
+		aluno.nome = "João";
+		lista.adicionar(aluno);
+		
+		aluno = new Aluno();
+		aluno.nome = "Maria";
+		lista.adicionar(aluno);
+		
+		aluno = new Aluno();
+		aluno.nome = "Alexandre";
+		lista.adicionar(aluno);
+		
+		aluno = new Aluno();
+		aluno.nome = "Normandes";
+		lista.adicionar(aluno);
+		
+		aluno = new Aluno();
+		aluno.nome = "Thiago";
+		lista.adicionar(aluno);
+		
+		for (int i = 0; i < lista.tamanho(); i++) {
+			Aluno a = lista.obter(i);
+			System.out.println("Aluno: " + a.nome);
+		}
+	}
+
+}
