54e4dea8782dac7ff7b8218a945fe11dddfd44a4
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-02 21:51:37 -0300

    Add files via upload

---
+++ utilizando_codigo_de_outros_programadores/Carteiro.java
@@ -0,0 +1,9 @@
+package utilizando_codigo_de_outros_programadores;
+
+public class Carteiro {
+        static void enviar() throws EmailException {
+            Email email = new SimpleEmail();
+
+        }
+    }
+}
