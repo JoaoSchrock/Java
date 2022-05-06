9fd3436e27d43cd1a9f2b1f8375c20d8216670c6
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-05 21:45:12 -0300

    Add files via upload

---
+++ acessando_Atributos_de_Objetos/Carro/Proprietario.java
@@ -0,0 +1,11 @@
+package acessando_Atributos_de_Objetos.composicaoObjetos;
+
+public class Proprietario {
+    String nome;
+    String CPF;
+    int idade;
+    String logradouro;
+    String bairro;
+    String cidade;
+
+}
