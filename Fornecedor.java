a7855d1d82b8878a62ec0a4f65d7997ea4c85658
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-12 01:45:58 -0300

    Add files via upload

---
+++ Fornecedor.java
@@ -0,0 +1,13 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2.desafio_objeto_this_e_construtores;
+
+public class Fornecedor {
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
