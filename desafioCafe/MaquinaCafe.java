1a787e569365c7f06853e9a1848ad557935bdc9d
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-25 21:50:48 -0300

    Add files via upload

---
+++ desafioCafe/MaquinaCafe.java
@@ -0,0 +1,17 @@
+package _5_18_sobrecaraga.desafioCafe;
+
+public class MaquinaCafe {
+    String acucarDisponivel = "2 kg";
+
+    public void fazerCafe(MaquinaCafe maquinaCafe) {
+        System.out.println("Temos: " + acucarDisponivel + " de Acucar.");
+        if(acucarDisponivel == "2"){
+            System.out.println("Capacidade de Acucar nivel maximo " + acucarDisponivel);
+        }else if(acucarDisponivel == "1"){
+            System.out.println("Pouca acucar, Precisa repor se nao sem cafezinho por hoje");
+        }
+    }
+    public void fazerCafe() {
+    }
+    static void 
+}
---
+++ desafioCafe/Testecafe.java
@@ -0,0 +1,9 @@
+package _5_18_sobrecaraga.desafioCafe;
+
+public class Testecafe extends MaquinaCafe {
+    public static void main(String[] args) {
+        System.out.println("Voce quer cafe?");
+
+
+    }
+}
