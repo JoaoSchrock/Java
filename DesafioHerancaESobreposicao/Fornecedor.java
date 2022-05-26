8971ea47a2cfdea280741dbb79c9cf9268f21730
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-05-25 21:12:58 -0300

    Delete DesafioHerancaESobreposicao directory

--- DesafioHerancaESobreposicao/Fornecedor.java
+++
@@ -1,9 +0,0 @@
-package DesafioHerancaESobreposicao;
-
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-public class Fornecedor extends Pessoa {
-    String nome;
-    protected int idade;
-
-}
