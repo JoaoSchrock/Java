3828861c89be7f078b7d9e8223a8d9c445cc3350
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-06-07 20:37:53 -0300

    Delete DesafioInterfaceePolimorfismo directory

--- DesafioInterfaceePolimorfismo/CorretoraSeguros.java
+++
@@ -1,8 +0,0 @@
-package DesafioInterfaceePolimorfismo;
-
-public class CorretoraSeguros {
-
-    public void fazerPropostaSeguro(CorretoraSeguros carro) {
-        System.err.println("Nossa Corretora de Seguros\nTem uma Oferta Especial para Voçê!");
-    }
-}
--- DesafioInterfaceePolimorfismo/Exercicio PROF/Seguravel.java
+++
@@ -1,8 +0,0 @@
-package com.company.orientacao_a_objetos.orientacao_a_objetos_parte2._5_24_exercicio_interface_e_polimorfismo;
-
-public interface Seguravel {
-
-    public double calcularValorApolice();
-
-    public String obterDescricao();
-}
