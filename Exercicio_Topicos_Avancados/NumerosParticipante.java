93137b0d85d3446d2284e57cc167eda8ea0834ed
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-06-08 21:38:27 -0300

    Add files via upload

--- Exercicio_Topicos_Avancados/NumerosParticipante.java
+++ Exercicio_Topicos_Avancados/NumerosParticipante.java
@@ -1,14 +1,12 @@
 package Exercicio_Topicos_Avancados;
 
-
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.Scanner;
 
 public class NumerosParticipante {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("         _ MEGA SENA _");
+
+        System.out.println("        _ MEGA SENA _");
         System.out.println("" +
                 "|01|02|03|04|05|06|07|08|09|10|\n" +
                 "|11|12|13|14|15|16|17|18|19|20|\n" +
@@ -17,11 +15,38 @@ public class NumerosParticipante {
                 "|41|42|43|44|45|46|47|48|49|50|\n" +
                 "|51|52|53|54|55|56|57|58|59|60|");
 
-        System.out.println("Escolha 5 numeros de 1 a 60: ");
-        for(int i = 1; i <= 5;i++){
-            System.out.println("Numero " + i + ": ");
-            double numeros = scanner.nextDouble();
+        System.out.println("Quantos Jogos você deseja Jogar?");
+        int quantidadeJogos = scanner.nextInt();
+
+        for (int K = 1; K <= quantidadeJogos; K++) {
+            System.out.println("_ JOGO " + K + " _");
+
+            double numeroAleatorio1 = Math.random() * 60;
+            double numeroAleatorio2 = Math.random() * 60;
+            double numeroAleatorio3 = Math.random() * 60;
+            double numeroAleatorio4 = Math.random() * 60;
+            double numeroAleatorio5 = Math.random() * 60;
+            double numeroAleatorio6 = Math.random() * 60;
+
+
+
+            System.out.println("Nº: " + Math.round(numeroAleatorio1));
+            System.out.println("Nº: " + Math.round(numeroAleatorio2));
+            System.out.println("Nº: " + Math.round(numeroAleatorio3));
+            System.out.println("Nº: " + Math.round(numeroAleatorio4));
+            System.out.println("Nº: " + Math.round(numeroAleatorio5));
+            System.out.println("Nº: " + Math.round(numeroAleatorio6));
+
         }
+
     }
+
+
 }
+
+
+
+
+
+
 
