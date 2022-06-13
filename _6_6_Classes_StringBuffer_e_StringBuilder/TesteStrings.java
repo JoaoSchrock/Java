4912e29b58990465f6619ae06f887fdc349b263f
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-06-13 19:13:52 -0300

    Add files via upload

--- _6_6_Classes_StringBuffer_e_StringBuilder/TesteStrings.java
+++ _6_6_Classes_StringBuffer_e_StringBuilder/TesteStrings.java
@@ -1,4 +1,20 @@
-package com.company.orientacao_a_objetos._6_topicos_avancados._6_6_Classes_StringBuffer_e_StringBuilder;
+package _6_6_Classes_StringBuffer_e_StringBuilder;
 
 public class TesteStrings {
+    public static void main(String[] args) {
+        String s = "Olá";
+        s = s + " Pessoal!";
+        System.out.println(s);
+
+        // Jeito bom, Porém nao e o jeito indicado, entao esqueca isso e use o de baixo.
+
+        System.out.println("--------------------------------");
+
+        StringBuilder sb = new StringBuilder("Olá"); // Jeito mais eficiente de se utilizar e é bem mais facil de se Manipular,
+        sb.append(" Pessoal!");//Append traduzindo siguinifica "Acrescentar",no caso acrescenta a tal Frase.
+
+        String resultado = sb.toString();//to String converte para String.
+        System.out.println("com StringBuilder: " + resultado);
+
+    }
 }
