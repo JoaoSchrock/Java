24b66952f25d1db5fb5f4b945649baed361d1c49
Schrock Holmes <101228590+JoaoSchrock@users.noreply.github.com>
2022-06-13 20:34:37 -0300

    Add files via upload

--- _6_7_trabalhando_com_datas/TesteCalendar.java
+++ _6_7_trabalhando_com_datas/TesteCalendar.java
@@ -1,4 +1,29 @@
 package _6_7_trabalhando_com_datas;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TesteCalendar {
+    public static void main(String[] args) {
+
+        Calendar c = new GregorianCalendar();
+
+        c.set(Calendar.DAY_OF_MONTH, 31);
+        c.set(Calendar.MONTH, 0);
+        c.set(Calendar.YEAR, 2010);
+        c.set(Calendar.MINUTE, 30);
+        c.set(Calendar.SECOND, 0);
+
+        DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        System.out.println(formatador.format(c.getTime()));
+
+        c.add(Calendar.MONTH, 1);
+        System.out.println(formatador.format(c.getTime()));
+
+        c.roll(Calendar.DAY_OF_MONTH, 1);
+        System.out.println(formatador.format(c.getTime()));
+    }
 }
